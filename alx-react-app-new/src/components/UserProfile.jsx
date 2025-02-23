8e944baf174ebcbfbc687dd8c2c16ff2634a125d
jean pierre <habipi05@yahoo.fr>
2025-02-23 18:21:07 +0200

    blue color

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const UserProfile = (props) => {
   return (
-    <div style={{ border: '1px solid #ccc', borderRadius: '8px', padding: '20px', margin: '20px', backgroundColor: '#f9ffff', boxShadow: '3px 3px 5px #ddd' }}>
-      <h2 style={{ color: 'teal', fontSize: '24px', marginBottom: '10px' }}>{props.name}</h2>
+    <div style={{ border: '1px solid #ccc', borderRadius: '8px', padding: '20px', margin: '20px', backgroundColor: '#f9f9f9', boxShadow: '3px 3px 5px #ddd' }}>
+      <h2 style={{ color: 'blue', fontSize: '24px', marginBottom: '10px' }}>{props.name}</h2>
       <p style={{ fontSize: '16px', marginBottom: '5px' }}>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
       <p style={{ fontSize: '16px', color: '#555' }}>Bio: {props.bio}</p>
     </div>
