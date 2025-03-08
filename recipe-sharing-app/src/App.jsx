5de1ff76b81ba7e6e99a2388ced8db7896c07936
jean pierre <habipi05@yahoo.fr>
2025-03-08 17:47:16 +0200

    updated the recipeStore file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/AddRecipeForm'
+import React from 'react';
+import RecipeList from './components/RecipeList';
 import './app.css';
 
 function App() {
