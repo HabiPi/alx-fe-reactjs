7a3f78d21c812d5495e57185d5547dc8b1967864
jean pierre <habipi05@yahoo.fr>
2025-03-08 12:04:07 +0200

    Recipe List created

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import useRecipeStore from "../store/recipeStore";
+
+const RecipeList = () => {
+    const recipes = useRecipeStore((state) => state.recipes);
+
+    if (recipes || recipes.length === 0) {
+        return <p>No recipes available. Add one!</p>;
+    }
+}
+
+return (
+    <div>
+        <h2>recipes</h2>
+        <ul>
+            {recipes.map(recipe, index) => (
+                <li key={index}><h3>{recipe.title}</h3>
+                <p>Ingredients: {recipe.ingredients}</p>
+                <p>Instructions: {recipe.instructions}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+    );
+};
+
+export default RecipeList;
