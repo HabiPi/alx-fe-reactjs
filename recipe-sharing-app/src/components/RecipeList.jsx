0dcf177065808bcc6624d1e45c5492d2739fd1f9
jean pierre <habipi05@yahoo.fr>
2025-03-08 14:25:59 +0200

    Modified the RecipeStore location

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -7,20 +7,19 @@ const RecipeList = () => {
     if (recipes || recipes.length === 0) {
         return <p>No recipes available. Add one!</p>;
     }
-}
 
-return (
-    <div>
-        <h2>recipes</h2>
-        <ul>
-            {recipes.map(recipe, index) => (
-                <li key={index}><h3>{recipe.title}</h3>
-                <p>Ingredients: {recipe.ingredients}</p>
-                <p>Instructions: {recipe.instructions}</p>
-                </li>
-            ))}
-        </ul>
-    </div>
+    return (
+        <div>
+            <h2>recipes</h2>
+            <ul>
+                {recipes.map((recipe, index) => (
+                    <li key={index}><h3>{recipe.title}</h3>
+                        <p>Ingredients: {recipe.ingredients}</p>
+                        <p>Instructions: {recipe.instructions}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
