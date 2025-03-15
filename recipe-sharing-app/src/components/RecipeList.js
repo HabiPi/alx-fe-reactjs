1a2761edcf8181bdcf6273e65aa0f656f929d1f9
jean pierre <habipi05@yahoo.fr>
2025-03-15 10:32:19 +0200

    Updated the recipe list file

---
+++ recipe-sharing-app/src/components/RecipeList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipeList;
