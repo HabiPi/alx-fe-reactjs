96bbd5d7d4beed8d6d2601a1511238e35ed7a149
jean pierre <habipi05@yahoo.fr>
2025-03-15 11:37:05 +0200

    modified app.jsx file and the addrecipeform

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
-import './App.css';
-
 
 function App() {
   return (
-    <BrowserRouter> {}
-    <div className="App">
-      <h1>Recipe App</h1>
-      <Routes> {}<Route path="/" element={
-      <>
-      <RecipeList />
+    <div>
+      <h1>Recipe Manager</h1>
       <AddRecipeForm />
-      </>
-      } /> {}
-      <Route path="/recipes/:recipeId" element={<RecipeDetails />} /> {}
-      </Routes>
-      </div>
-      </BrowserRouter>
+      <RecipeList />
+    </div>
   );
 }
 
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Router, RouterProvider } from 'react-router-dom'
-import router from './router/router.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
