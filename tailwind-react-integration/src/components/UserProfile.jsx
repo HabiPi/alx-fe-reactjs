0c20a35a9a717be845b0197a2f2a261cd9e6c7bf
jean pierre <habipi05@yahoo.fr>
2025-03-17 16:49:59 +0200

    updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
 
     return (
-      <div className="bg-gray-100 p-8 sm:p-4 max-w-sm-400 mx-auto my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 md:p-8 sm:p-4 max-w-sm-400 mx-auto my-20 rounded-lg shadow-lg">
         <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User"/>
         <h1 className="text-xl sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
